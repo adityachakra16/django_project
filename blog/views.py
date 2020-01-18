90a83ba2c6c927d10e6efd4f73d132ee64f5c736
Aditya Chakraborty <31283807+adityachakra16@users.noreply.github.com>
2020-01-18 18:01:11 -0500

    Use post class in views.py and admin.py

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Jan 18, 2020'
-    },
-    {
-        'author': 'Jane',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Jan 19, 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
